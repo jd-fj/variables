4a8d10ca6c6b359b137ffce37a71f28b7bfe5e0c
taylor d <taylor.delph@eyecuelab.com>
2022-04-03 11:42:49 -0700

    add integer type notes

---
+++ src/scalar_types/integer.rs
@@ -0,0 +1,8 @@
+// integers types can be signed or unsigned. Signed integers use the i prefix, unsigned integers use the u prefix.
+
+// 8-bit: i8, u8
+// 16-bit: i16, u16
+// 32-bit: i32, u32
+// 64-bit: i64, u64
+// 128-bit: i128, u128
+// arch: isize, usize
